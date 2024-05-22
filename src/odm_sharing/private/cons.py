304914e9ddafab5329f09fe284ed7c6ced9d77ee
zargot <zargot.ripley@gmail.com>
2024-05-22 12:56:23 -0400

    cons: finish impl
    - add dialect name func
    - add args to exec func
    - add table whitelist to excel connect, to improve import performance
      when not all sheets are used
    - print excel import info
    - fix excel connection
    - add exception handling

--- src/odm_sharing/private/cons.py
+++ src/odm_sharing/private/cons.py
@@ -1,32 +1,66 @@
-from typing import cast
+from typing import List, cast
 
 import pandas as pd
 import sqlalchemy as sa
 
+
 Connection = object  # opaque data-source connection handle
 
 
-def connect_excel(path: str) -> Connection:
+class DataSourceError(Exception):
+    pass
+
+
+def _connect_excel(path: str, tables: List[str]) -> Connection:
+    ''':raises OSError:'''
     # copies excel data to in-memory db, to abstract everything as a db
-    data = pd.read_excel(path)
+    print('importing excel workbook')
+    table_whitelist = set(tables)
     db = sa.create_engine('sqlite://', echo=False)
-    for table in data.keys():
-        data.to_sql(table, db, index=False)
+    xl = pd.ExcelFile(path)
+    included_tables = set(map(str, xl.sheet_names)) & table_whitelist
+    for table in included_tables:
+        print(f'- table {table}')
+        df = xl.parse(sheet_name=table)
+        df.to_sql(table, db, index=False, if_exists='replace')
     return cast(Connection, db)
 
 
-def connect_db(url: str) -> Connection:
+def _connect_db(url: str) -> Connection:
+    ''':raises sa.exc.OperationalError:'''
     return sa.create_engine(url)
 
 
-def connect(data_source: str) -> Connection:
-    if data_source.endswith('.xlsx'):
-        return connect_excel(data_source)
-    else:
-        return connect_db(data_source)
+def connect(data_source: str, tables: List[str] = []) -> Connection:
+    '''
+    connects to a data source and returns the connection
+
+    :param tables: when connecting to an excel file, this acts as a sheet
+        whitelist
+
+    :raises DataSourceError:
+    '''
+    try:
+        if data_source.endswith('.xlsx'):
+            return _connect_excel(data_source, tables)
+        else:
+            return _connect_db(data_source)
+    except (OSError, sa.exc.OperationalError) as e:
+        raise DataSourceError(str(e))
 
 
-def exec(c: Connection, sql: str) -> pd.DataFrame:
-    '''executes `sql` on connection `c`'''
+def get_dialect_name(c: Connection) -> str:
+    '''returns the name of the dialect used for the connection'''
+    return cast(sa.engine.Engine, c).dialect.name
+
+
+def exec(c: Connection, sql: str, sql_args: List[str] = []) -> pd.DataFrame:
+    '''executes sql with args on connection
+
+    :raises DataSourceError:
+    '''
     db = cast(sa.engine.Engine, c)
-    return pd.read_sql_query(sql, db)
+    try:
+        return pd.read_sql_query(sql, db, params=tuple(sql_args))
+    except sa.exc.OperationalError as e:
+        raise DataSourceError(str(e))
