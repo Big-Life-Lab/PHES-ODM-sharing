9844f888c62f27c2e05789d709b7f327b15614ee
zargot <zargot.ripley@gmail.com>
2024-07-25 16:38:10 -0400

    doc: factor out asset-copy code

--- docs/manual/common.py
+++ docs/manual/common.py
@@ -1,9 +1,16 @@
+import inspect
+from os.path import abspath, dirname, join
+from shutil import copy2
+from typing import List
+
 import pandas as pd
 from tabulate import tabulate
 
 
-SCHEMA = 'assets/minimal/schema.csv'
-DATA = 'assets/minimal/measures.csv'
+MODULE_DIR = dirname(abspath(inspect.getfile(inspect.currentframe())))
+ASSET_DIR = join(MODULE_DIR, 'assets/minimal/')
+SCHEMA = join(ASSET_DIR, 'schema.csv')
+DATA = join(ASSET_DIR, 'measures.csv')
 
 
 def load_csv_md(path):
@@ -16,3 +23,9 @@ def load_csv_md(path):
 def print_file(path):
     with open(path, 'r') as f:
         print(f.read())
+
+
+def copy_assets(filenames: List[str]) -> None:
+    '''copies asset to current directory to avoid long paths in examples'''
+    for fn in filenames:
+        copy2(join(ASSET_DIR, fn), fn)
