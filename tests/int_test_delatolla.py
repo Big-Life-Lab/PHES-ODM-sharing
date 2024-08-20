5aed19cf683f33de175a114140c57d9ac1f8f455
zargot <zargot.ripley@gmail.com>
2024-08-20 10:50:39 -0400

    test: add integration test

---
+++ tests/int_test_delatolla.py
@@ -0,0 +1,27 @@
+import unittest
+from os.path import join
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from odm_sharing.tools.share import OutFmt, share
+
+from common import OdmTestCase, readfile
+
+
+class IntTestDelatolla(OdmTestCase):
+    def test_excel_delatolla(self) -> None:
+        delatolla_dir = join(self.dir, 'int', 'delatolla')
+        schema_path = join(delatolla_dir, 'schema.csv')
+        data_path = join(delatolla_dir, 'data.xlsx')
+        with TemporaryDirectory() as tmpdir:
+            paths = share(schema_path, data_path, outdir=tmpdir,
+                          outfmt=OutFmt.CSV)
+            for path in paths:
+                actual = readfile(path)
+                fn = Path(path).name
+                expected = readfile(join(delatolla_dir, 'expected', fn))
+                self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
